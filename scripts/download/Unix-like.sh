76d93293fa47d59540fb835aaa4fe34b6de3693e
Leo Diegues <leonardo.diegues@grupofolha.com.br>
2025-02-25 18:38:56 -0300

    fix: Enhance debug output for FFmpeg and FFprobe downloads in Unix-like script

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -1,38 +1,49 @@
 #!/bin/sh
 set -eu
 echo ::group::Downloading FFmpeg $version for $RUNNER_OS $RUNNER_ARCH
-mkdir -p FFmpeg
+mkdir FFmpeg
 if [ $RUNNER_OS = macOS ]; then
   if [ $RUNNER_ARCH = ARM64 ]; then
-    ext=zip
-    if [ $version = master ]; then
-      echo ::error::OSXExperts.NET currently does not have macOS ARM64 master builds.
-      exit 1
-    else
-      echo "https://www.osxexperts.net/ffmpeg${version}arm.zip"
-      echo "https://www.osxexperts.net/ffprobe${version}arm.zip"
-      wget -qO FFmpeg.zip https://www.osxexperts.net/ffmpeg${version}arm.zip
-      wget -qO FFprobe.zip https://www.osxexperts.net/ffprobe${version}arm.zip
-    fi
+	ext=zip
+	echo ::debug::Setting extension to "zip"
+	if [ $version = master ]; then
+	  echo ::error::OSXExperts.NET currently does not have macOS ARM64 master builds.
+	  exit 1
+	else
+	  echo ::debug::Downloading FFmpeg from OSXExperts.NET
+	  echo ::debug::Download URLs:
+	  echo ::debug::https://www.osxexperts.net/ffmpeg${version}arm.zip
+	  echo ::debug::https://www.osxexperts.net/ffprobe${version}arm.zip
+	  wget -qO FFmpeg.zip https://www.osxexperts.net/ffmpeg${version}arm.zip
+	  wget -qO FFprobe.zip https://www.osxexperts.net/ffprobe${version}arm.zip
+	fi
   else
-    ext=7z
-    if [ $version = master ]; then
-      wget -qO FFmpeg.7z https://evermeet.cx/ffmpeg/get
-      wget -qO FFprobe.7z https://evermeet.cx/ffmpeg/get/ffprobe
-    else
-      wget -qO FFmpeg.7z https://evermeet.cx/ffmpeg/ffmpeg-$version.7z
-      wget -qO FFprobe.7z https://evermeet.cx/ffprobe/ffprobe-$version.7z
-    fi
+	ext=7z
+	echo ::debug::Setting extension to "7z"
+	echo ::debug::Downloading FFmpeg from Evermeet.cx
+	if [ $version = master ]; then
+		echo ::debug::Download URLs:
+		echo ::debug::https://evermeet.cx/ffmpeg/get
+		echo ::debug::https://evermeet.cx/ffmpeg/get/ffprobe
+	  wget -qO FFmpeg.7z https://evermeet.cx/ffmpeg/get
+	  wget -qO FFprobe.7z https://evermeet.cx/ffmpeg/get/ffprobe
+	else
+		echo ::debug::Download URLs:
+		echo ::debug::https://evermeet.cx/ffmpeg/ffmpeg-$version.7z
+		echo ::debug::https://evermeet.cx/ffprobe/ffprobe-$version.7z
+	  wget -qO FFmpeg.7z https://evermeet.cx/ffmpeg/ffmpeg-$version.7z
+	  wget -qO FFprobe.7z https://evermeet.cx/ffprobe/ffprobe-$version.7z
+	fi
   fi
+  echo ::debug::Extracting FFmpeg and FFprobe
   7z e FFmpeg.$ext ffmpeg -oFFmpeg
   7z e FFprobe.$ext ffprobe -oFFmpeg
+  echo ::debug::Removing FFmpeg and FFprobe original files
   rm FFmpeg.$ext FFprobe.$ext
-  echo ls -a FFmpeg
 else
   if [ $RUNNER_ARCH = ARM64 ]; then arch=linuxarm64; else arch=linux64; fi
   if [ $version = master ]; then filename=ffmpeg-master-latest-$arch-gpl.tar.xz; else filename=ffmpeg-n$version-latest-$arch-gpl-$version.tar.xz; fi
   wget -qO- $GITHUB_SERVER_URL/BtbN/FFmpeg-Builds/releases/download/latest/$filename | tar -xJC FFmpeg --strip-components 2 --no-anchored ffmpeg ffprobe
 fi
-echo ls -a FFmpeg
 echo ::endgroup::
 
