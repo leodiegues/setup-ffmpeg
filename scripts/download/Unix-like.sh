95c5d2743b27a67db3886d3eccd03df2a3244172
Leo Diegues <leonardo.diegues@grupofolha.com.br>
2025-02-25 18:50:53 -0300

    chore: add debugging echo for directory listing in Unix-like.sh

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 echo "::group::Downloading FFmpeg $version for $RUNNER_OS $RUNNER_ARCH"
+echo ls -a
 mkdir FFmpeg
 if [ $RUNNER_OS = macOS ]; then
   if [ $RUNNER_ARCH = ARM64 ]; then
