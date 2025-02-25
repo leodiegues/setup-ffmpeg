be61cf60ab519eb8349b0e270a4d503e724cbc18
Leo Diegues <leonardo.diegues@grupofolha.com.br>
2025-02-25 19:32:35 -0300

    fix: minor

--- scripts/download/Unix-like.sh
+++ scripts/download/Unix-like.sh
@@ -10,8 +10,8 @@ if [ $RUNNER_OS = macOS ]; then
       exit 1
     else
       echo "https://www.osxexperts.net/ffmpeg${version}arm.zip"
-      curl -H "User-Agent: Mozilla/5.0" -L -o FFmpeg.zip https://www.osxexperts.net/ffmpeg${version}arm.zip
-      curl -H "User-Agent: Mozilla/5.0" -L -o FFprobe.zip https://www.osxexperts.net/ffprobe${version}arm.zip
+      curl -H "User-Agent: Mozilla/5.0" -v -L -o FFmpeg.zip https://www.osxexperts.net/ffmpeg${version}arm.zip
+      curl -H "User-Agent: Mozilla/5.0" -v -L -o FFprobe.zip https://www.osxexperts.net/ffprobe${version}arm.zip
     fi
   else
     ext=7z
